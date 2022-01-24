dc66853dcb9ea59ab3f9d6f0512ef663e8d53f83
Artem <temoha1386@gmail.com>
2022-01-24 22:52:43 +0200

    Added favicon

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -6,6 +6,11 @@
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
+<link rel="apple-touch-icon" sizes="180x180" href="{{ asset('img/favicon/apple-touch-icon.png') }}">
+<link rel="icon" type="image/png" sizes="32x32" href="{{ asset('img/favicon/favicon-32x32.png') }}">
+<link rel="icon" type="image/png" sizes="16x16" href="{{ asset('img/favicon/favicon-16x16.png')}} ">
+<link rel="manifest" href="{{ asset('img/favicon/site.webmanifest')}}">
+
    <meta property="og:url" content="https://клининг.dp.ua/" />
    <meta property="og:type" content="website" />
    <meta property="og:title" content="@yield('ogTitle')" />
