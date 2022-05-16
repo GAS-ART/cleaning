5618add0f41bbe6c0e4f923448e17f589e678642
Artem <temoha1386@gmail.com>
2022-05-16 16:27:46 +0300

    Changed contacts

--- resources/views/inc/footer.blade.php
+++ resources/views/inc/footer.blade.php
@@ -7,12 +7,12 @@
                   <h4>контакты</h4>
                </div>
                <div class="contacts-phone">
-                  <a href="tel:0981183119">(098)118-31-19</a>
+               <a href="tel:0976394515">(097)639-45-15</a>
                </div>
                <div class="contacts-phone">
                   <a href="tel:0636642636">(063)664-26-36</a>
                </div>
-               <div class="contacts-email">
+               <div class="contacts-email-footer">
                 <a href="mailto:dnepr.jc@gmail.com">dnepr.jc@gmail.com</a>
                </div>
             </div>
